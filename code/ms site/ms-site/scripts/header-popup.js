96acf8be5c271d68d57bb9be18b9f51c6db685b2
kabage mburu <kabagemburu@yahoo.com>
2022-06-15 10:01:53 +0300

    Delete the initial commit

---
+++ code/ms site/ms-site/scripts/header-popup.js
@@ -0,0 +1,14 @@
+ 
+
+document.addEventListener("DOMContentLoaded", function () {
+    var allMicrosoftBtn = document.querySelector("#all_microsoft_btn");
+
+    allMicrosoftBtn.addEventListener("click", function () {
+        document.getElementById("popup").classList.toggle("open");
+    })
+
+})
+    
+
+
+
